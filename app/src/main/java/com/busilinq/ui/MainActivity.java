f199b9d9d361b8c1f758b210ecf2db2b894fd8c9
陈永祥 <865877521@qq.com>
2018-02-08 16:21:30 +0800

    修改登录返回刷新 界面

--- app/src/main/java/com/busilinq/ui/MainActivity.java
+++ app/src/main/java/com/busilinq/ui/MainActivity.java
@@ -207,11 +207,11 @@ public class MainActivity extends BaseActivity {
         if (ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
         }
-//        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
-//                || checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-//            permissions.add(Manifest.permission.CAMERA);
-//            permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
-//        }
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                || checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.CAMERA);
+            permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
 
         //读写权限和电话状态权限非必要权限(建议授予)只会申请一次，用户同意或者禁止，只会弹一次
         // 读写权限
