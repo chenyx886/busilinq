82d00c12720be51f9f3a1e8bb7912b7749dd1e03
zhengxin.yu <zhengxin.yu@busilinq.com>
2018-03-01 15:56:32 +0800

    新增支付选择界面

---
+++ app/src/main/java/com/busilinq/presenter/mine/order/PaymentPresenter.java
@@ -0,0 +1,20 @@
+package com.busilinq.presenter.mine.order;
+
+import com.busilinq.contract.mine.order.IPaymentView;
+import com.busilinq.presenter.BasePresenter;
+
+/**
+ * Company：华科建邺
+ * Class Describe：
+ * Create Person：yuzhenxing
+ * Create Time：2018/3/1 14:55
+ * Update Person：
+ * Update Time：
+ * Update Remark：
+ */
+public class PaymentPresenter extends BasePresenter<IPaymentView> {
+
+    public PaymentPresenter(IPaymentView mvpView) {
+        super(mvpView);
+    }
+}
---
+++ app/src/main/java/com/busilinq/ui/mine/order/PaymentActivity.java
@@ -0,0 +1,100 @@
+package com.busilinq.ui.mine.order;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.busilinq.R;
+import com.busilinq.base.BaseMvpActivity;
+import com.busilinq.contract.mine.order.IPaymentView;
+import com.busilinq.presenter.mine.order.PaymentPresenter;
+import com.busilinq.ui.ToDevelopedActivity;
+import com.busilinq.widget.IconTextItem;
+import com.chenyx.libs.utils.JumpUtil;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+/**
+ * Company：华科建邺
+ * Class Describe：
+ * Create Person：yuzhenxing
+ * Create Time：2018/3/1 14:55
+ * Update Person：
+ * Update Time：
+ * Update Remark：
+ */
+public class PaymentActivity extends BaseMvpActivity<PaymentPresenter> implements IPaymentView {
+    /**
+     * 标题
+     */
+    @BindView(R.id.tv_title)
+    TextView mTitle;
+    /**
+     * 银联支付
+     */
+    @BindView(R.id.it_pay_unionpay)
+    IconTextItem itPayUnionpay;
+    /**
+     * 微信支付
+     */
+    @BindView(R.id.it_pay_wechat)
+    IconTextItem itPayWechat;
+    /**
+     * 支付宝支付
+     */
+    @BindView(R.id.it_pay_alipay)
+    IconTextItem itPayAlipay;
+    /**
+     * 标题
+     */
+    @BindView(R.id.it_transfer_accounts)
+    IconTextItem itTransferAccounts;
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_payment);
+    }
+    @OnClick({R.id.tv_back, R.id.it_pay_unionpay, R.id.it_pay_wechat,R.id.it_pay_alipay,R.id.it_transfer_accounts})
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_back:
+                finish();
+                break;
+            case R.id.it_pay_unionpay:
+                JumpUtil.overlay(this, ToDevelopedActivity.class);
+                break;
+            case R.id.it_pay_wechat:
+                JumpUtil.overlay(this, ToDevelopedActivity.class);
+                break;
+            case R.id.it_pay_alipay:
+                JumpUtil.overlay(this, ToDevelopedActivity.class);
+                break;
+            case R.id.it_transfer_accounts:
+                JumpUtil.overlay(this, ToDevelopedActivity.class);
+                break;
+        }
+    }
+    @Override
+    protected PaymentPresenter createPresenter() {
+        return null;
+    }
+
+    @Override
+    protected void initUI() {
+        mTitle.setText("选择支付方式");
+    }
+    @Override
+    public void showProgress(String message) {
+
+    }
+
+    @Override
+    public void hideProgress() {
+
+    }
+
+
+}
